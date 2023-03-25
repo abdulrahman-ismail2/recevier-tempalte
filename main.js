d369a29204d27f612588195d723914ca7b22bbc1
abdulrahman-ismail2 <128198179+abdulrahman-ismail2@users.noreply.github.com>
2023-03-25 17:28:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// let chk = document.getElementById("chk-box"),
+// links = document.querySelector(".links")
+// chk.onclick = () => {
+//     if(links.classList.contains('hide')){
+//         links.classList.remove('hide');
+//     }else{
+//         links.classList.add('hide');
+//         // document.body.style.overflowX = "hidden";
+//     }
+    
+    
+// }
